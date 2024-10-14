e45ded01b56f3a42b50376b464ce687172854d45
colenocks <enockscoleman@gmail.com>
2024-10-14 15:27:09 +0100

    feat: install react query and dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
-import theme from './theme'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import theme from "./theme";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // how many times after initial request to retry,
+      cacheTime: 300_000, // 5 min -> How long before the cache is considered inactive (i.e if no "observer" is using the cache) to be garbage collected.
+      staleTime: 3 * 10 * 1000, // 3 min -> How long the data is considered fresh. 0 -> data is treated as old the moment we get it.
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
